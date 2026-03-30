8401986a0b54f06b7393ce5527d7e12d3a841b47
El-cmd <lothvalentin@gmail.com>
2026-03-30 10:50:25 +0200

    Add full testimonial texts

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -6,7 +6,7 @@ export const testimonials: TestimonialItem[] = [
     date: '14 avr. 2025',
     category: 'Bricolage et multi-services',
     quote:
-      "Intervention très pédagogique, avec des explications claires à chaque étape. Tableau électrique presque remis à neuf, prises de cuisine reprises proprement, et un résultat final pleinement fonctionnel.",
+      "Très content de l'intervention de Quentin. Très pédagogue, il explique dans le moindre détail comment il procède sur chaque étape. Je lui ai demandé d'intervenir sur mon tableau électrique, et il l'a presque remis à neuf. Il m'a aussi changé toutes les prises de courant de la cuisine, et malgré quelques imprévus dû à un peu à la vétusté de mon installation, tout a été fait correctement et tout est de nouveau fonctionnel. Je ne peux que recommander vivement ses services et son professionnalisme.",
     highlight: 'Expert dans son domaine',
   },
   {
@@ -14,7 +14,7 @@ export const testimonials: TestimonialItem[] = [
     date: '28 oct. 2025',
     category: 'Installation électrique',
     quote:
-      "Très sympa, prix juste et transparent, sans mauvaise surprise. Le petit plus qui rassure : une vidéo en fin d'intervention pour montrer le travail terminé.",
+      "Quentin est un électricien au top ! Très sympa, il a pris le temps d'expliquer chaque étape. Prix juste et transparent, pas de mauvaise surprise. Cerise sur le gâteau : il a réalisé une petite vidéo à la fin pour me montrer le travail terminé, super pro et rassurant. Je recommande à 100 % !",
     reply: 'Merci David pour votre retour.',
   },
   {
@@ -22,107 +22,107 @@ export const testimonials: TestimonialItem[] = [
     date: '13 juin 2025',
     category: 'Installation électrique',
     quote:
-      "Une personne sérieuse qui connaît son métier, disponible et ponctuelle. Une journée de travail intense, sans relâcher l'exigence sur la qualité.",
-    reply: 'Merci pour votre retour Michelle, avec plaisir.',
+      "Quentin est une personne sérieuse qui connaît son métier. Je le remercie pour la qualité de son travail, sa disponibilité et sa ponctualité. Il a passé la journée à travailler sans même prendre de pause. Je l’inscris sur ma liste d’intervenants compétents et n’hésiterai pas à le recommander.",
+    reply: 'Merci pour votre retour Michelle avec plaisir',
   },
   {
     name: 'Pascal L.',
     date: '13 janv. 2026',
     category: 'Installation électrique',
     quote:
-      "Très bien, super rapide sur le délai d'intervention, avec une efficacité immédiate pour résoudre le souci électrique. Une prestation nette et rassurante.",
+      "très bien. super rapide pour le délai d'intervention et efficacité absolu pour résoudre le souci électrique. je recommande",
     highlight: 'Super réactif',
-    reply: 'Merci pour votre retour Pascal.',
+    reply: 'Merci pour votre retour pascal',
   },
   {
-    name: 'Gerard L.',
-    date: '27 fevr. 2026',
+    name: 'Gérard L.',
+    date: '27 févr. 2026',
     category: 'Installation électrique',
     quote:
-      "Remplacement de tableau effectué avec méthode, minutie et soin jusqu'à la finalisation du chantier. Échange sympathique, respectueux et très professionnel.",
+      "changement tableau électrique, ponctuel, méthodique et minutieux, soigneux et patients dans son travail, non stop jusqu'à la finalité du chantier. Echange très sympathique et respectueux",
     highlight: 'Excellent rapport qualite/prix',
   },
   {
     name: 'Orlane G.',
-    date: '28 fevr. 2026',
+    date: '28 févr. 2026',
     category: 'Installation électrique',
     quote:
-      "Disponible, sérieuse et professionnelle. Elle souligne surtout la capacité à rester mobilisé jusqu'à résolution complète du problème, avec une communication claire du début à la fin.",
+      "Quentin est une personne disponible, sérieuse et professionnelle. Il donne son temps pour pouvoir résoudre nos problèmes. Grâce à ses compétences, sa communication, son expérience, il m'a réglé mon problème. C'est une personne qui vous ne lâchera pas tant que vous n'avez pas réglé votre problème. Merci beaucoup pour m'avoir aidé, je recommande vivement cet électricien. Vous pouvez le prendre sans soucis",
     highlight: 'Super reactif',
   },
   {
-    name: 'Utilisateur supprime',
+    name: 'Utilisateur supprimé',
     date: '05 sept. 2023',
     category: 'Installation électrique',
     quote:
-      "Très sympathique, fiable, ponctuel et compétent. Travail esthétique, de qualité, avec un vrai sens du service même en situation d'urgence.",
+      "Très sympathique, compréhensif, serviable, agréable, ponctuel, sérieux, fiable, et compétent. Ne cherchez plus, vous avez trouvé l'électricien qu'il vous faut ! Travail esthétique, de qualité, et a fait ce qu'il pouvait pour le faire au plus vite parce que j'étais dans l'urgence. Est même intervenu sur un travail qui ne le concernait pas pour me sortir d'un mauvais pas !! Franchement, je recommande à 100 % ! Merci Quentin.",
     reply:
-      "Merci Adeline pour votre retour et votre accueil, cela a ete un plaisir pour moi.",
+      "Merci Adeline pour votre retour et merci pour votre accueil ça a été un plaisir pour moi n'hésitez pas à me recontacter au besoin",
   },
   {
     name: 'Christine D.',
-    date: 'Avis recent',
+    date: 'mercredi',
     category: 'Installation électrique',
     quote:
-      "Cliente très satisfaite du sérieux, de la conscience professionnelle et du soin apporté au travail. Elle insiste aussi sur la gentillesse et l'envie de refaire appel aux services de Quentin.",
-    highlight: 'Tres soigneux',
+      "Je suis tout à fait satisfaite du travail de Quentin. C'est quelqu'un de très sérieux, consciencieux, compétent. Il a à cœur que son travail soit le plus parfait. De plus c'est une personne agréable, gentille. Merci à lui, je n'hésiterai pas à faire appel de nouveau pour de prochains travaux. Encore merci et j'ai vraiment rencontré une très belle personne",
+    highlight: 'Très soigneux',
   },
   {
     name: 'Martine S.',
     date: '23 juin 2023',
     category: 'Installation électrique',
     quote:
-      "Travail impeccable, solutions trouvées avec beaucoup de patience et une gentillesse constante. Un accueil client qui laisse un souvenir très positif.",
+      "Ne cherchez surtout pas ailleurs.... une perle Monsieur tres pro, travail impeccable, trouve une solution à tout problème et tout cela avec beaucoup de patience et de gentillesse sans oublier une politesse irréprochable",
     reply:
-      "C'est reciproque, merci Martine pour votre accueil et votre gentillesse.",
+      "C'est réciproque merci Martine pour votre accueil et votre gentillesse ça a été un plaisir pour moi. Cordialement",
   },
   {
     name: 'Fabien',
-    date: '21 aout 2023',
+    date: '21 août 2023',
     category: 'Installation électrique',
     quote:
-      "Tres reactif des la demande, devis etabli rapidement, tarif raisonnable et intervention qui va au-dela du cadre prevu pour resoudre aussi la panne complementaire.",
+      "Quentin est un électricien très professionnel, il a été très réactif dès ma demande déposée, il est venu très vite afin d’établir le devis. Devis réalisé dans la foulée et proposition acceptée Son intervention à été réalisée pour un prix très raisonnable il est venu avec du fil et des wagos qu il ne m’a pas facturés De plus il a m a installe des prises non prévues dans le devis initial et réalisé une recherche de panne qui n était pas prévue ,qu il a résolue Très pro et sympa je recommande",
     reply:
-      'Merci pour votre retour Fabien et pour votre accueil. Bonne continuation pour vos travaux.',
+      'Merci pour votre retour Fabien et pour votre accueil bonne continuation pour vos travaux',
   },
   {
     name: 'Mathieu',
-    date: '22 aout 2023',
-    category: "Artisan tout corps d'etat - Renovation",
+    date: '22 août 2023',
+    category: "Artisan tout corps d'état - Rénovation",
     quote:
-      "Tableau divisionnaire et appareillage realises avec soin. Le commentaire revient autant sur la qualite d'execution que sur la disponibilite et les bons conseils.",
+      "Quentin a installé un tableau divisionnaire pour l'étage de ma maison et fait l'appareillage dans les pièces. Travail soigné, personne sérieuse, hyper sympathique et serviable. Toujours disponible et de bons conseils. N'hésitez pas une seconde.",
     reply:
-      "Merci Matthieu pour ton retour, cela a ete un plaisir pour moi.",
+      'Merci Matthieu pour ton retour ça a été un plaisir pour moi bon courage pour tes futurs travaux',
   },
   {
     name: 'Assi L.',
     date: '26 janv. 2023',
     category: 'Installation électrique',
     quote:
-      "Un avis tres enthousiaste sur la conscience professionnelle, la ponctualite et la capacite a faire avancer les projets. Le prix est aussi percu comme tres abordable.",
+      "Le best du best le meilleur d’allo voisin je suis sure il travail bien consciencieux et ponctuel. Il m’a beaucoup aidé dans l’avancement de mes projets je ne sais pas comment remercier de monsieur . Je n’ai jamais rencontré une personne qui travail aussi bien. J’ai fais appel à ses services et je ne suis pas déçu dutout le abordable. Je vous remercie monsieur bocou",
   },
   {
     name: 'Bernard V.',
     date: '04 mars 2024',
     category: 'Installation électrique',
     quote:
-      "Contacte un dimanche pour un probleme complexe de va-et-vient, Quentin a repondu rapidement puis intervient des le lendemain. Le retour met en avant la rapidite, l'efficacite et le prix.",
+      "Bj à tous, nous avions un problème de vas et vient, avec des câbles électriques sans couleur sur un interrupteur et l'autre avec 6 fils dans la prise. Nous avons fait donc appel à Quentin par un message un dimanche, il a répondu au bout de 1H30, et il est passé le lendemain midi, Donc conclusion :rapidité d'action, efficacité, et la cerise sur le gâteau. le PRIX. Merci QUENTIN. nous n'hésiterons pas à faire appel à vos services",
   },
   {
     name: 'Pierre N.',
     date: '09 juil. 2024',
     category: 'Installation électrique',
     quote:
-      "Depannage en urgence jusqu'en fin de soiree, avec reperage de ligne complexe et rehabilitation complete du tableau. Un retour tres fort sur la reactivite et la competence.",
-    reply: 'Merci Pierre pour ton retour et ton accueil.',
+      "Au top à recommander à tous quentin et réactif et très compétent et venue le dépanné en urgence et a fini a 22h45. Pour tous ceux qui ont un problème comme j'ai eu ne chercher plus ailleurs. repérage de ligne installation complexe réhabilitation du tableaux complet .merci quentin",
+    reply: 'Merci Pierre pour ton retour ton accueil',
   },
   {
     name: 'Marie',
     date: '14 janv. 2025',
     category: 'Installation électrique',
     quote:
-      "Reponse ultra rapide, intervention tres professionnelle et vrai travail de recherche pour trouver une solution a un probleme qui paraissait insoluble.",
+      "Réponse ultra rapide. Intervention très pro et très sympathique. J'avais un problème qui semblait insoluble sur une installation électrique, Quentin a pris le temps pour trouver la solution qui n'était pas du tout évidente. Je recommande et lui dit bravo et merci",
     highlight: 'Expert dans son domaine',
-    reply: 'Merci pour votre retour Marie, a bientot.',
+    reply: 'Merci pour votre retour marie à bientôt',
   },
 ]
